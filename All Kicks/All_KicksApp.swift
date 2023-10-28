771a0aaa79d2363fd2f63f523b8d98c4c389831e
Koury Harmon <harmon.577@osu.edu>
2023-10-28 10:39:12 -0400

    Initial Commit

---
+++ All Kicks/All_KicksApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_KicksApp.swift
+//  All Kicks
+//
+//  Created by Koury Harmon on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct All_KicksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All KicksUITests/All_KicksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  All_KicksUITestsLaunchTests.swift
+//  All KicksUITests
+//
+//  Created by Koury Harmon on 10/28/23.
+//
+
+import XCTest
+
+final class All_KicksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
